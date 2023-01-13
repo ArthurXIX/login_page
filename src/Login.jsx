803ec8dc74722538ff0ab2d918e6afdf38e6ce61
ArthurXIX <88794540+ArthurXIX@users.noreply.github.com>
2023-01-13 15:04:24 -0600

    Form complete
    The form its already complete without css and javascript

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,26 @@
 import React, {useState} from "react";
 
-export const Login = () => {
+export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email)
+    }
+
     return (
-        <form action="">
-            <label for="email">Email:</label>
+        <>
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="email">Email:</label>
             <input value={email} type="email" placeholder="youremail@mail.com" id="email" name="email" />
 
-            <label for="password">Password:</label>
+            <label htmlFor="password">Password:</label>
             <input value={pass} type="password" placeholder="Your password" id="password" name="password" />
             <button>Log In</button>
         </form>
+        <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+        </>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,32 @@
-export const Register = () => {
+import React, {useState} from "react"
+
+
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email)
+    }
+
     return (
-        <>Register</>
+        <>
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="name">Nombre:</label>
+            <input value={name} type="text" placeholder="John Arthur" name="name" id="name" />
+
+            <label htmlFor="email">Email:</label>
+            <input value={email} type="email" placeholder="youremail@mail.com" id="email" name="email" />
+
+            <label htmlFor="password">Password:</label>
+            <input value={pass} type="password" placeholder="Your password" id="password" name="password" />
+            <button>Register</button>
+        </form>
+        <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+        </>
     )
 }
