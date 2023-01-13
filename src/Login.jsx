017ab0ab7c43132766a57fa6221408e398cf238f
ArthurXIX <88794540+ArthurXIX@users.noreply.github.com>
2023-01-13 11:10:11 -0600

    Primer commit

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+    return (
+        <form action="">
+            <label for="email">Email:</label>
+            <input type="email" placeholder="youremail@mail.com" id="email" name="email" />
+
+            <label for="password"></label>
+        </form>
+    )
+}
