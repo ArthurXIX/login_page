398a675f294354992abf702cd33d16db7f38e9ab
ArthurXIX <88794540+ArthurXIX@users.noreply.github.com>
2023-01-13 11:22:00 -0600

    Update login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,16 @@
+import React, {useState} from "react";
+
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
     return (
         <form action="">
             <label for="email">Email:</label>
-            <input type="email" placeholder="youremail@mail.com" id="email" name="email" />
+            <input value={email} type="email" placeholder="youremail@mail.com" id="email" name="email" />
 
-            <label for="password"></label>
+            <label for="password">Password:</label>
+            <input value={pass} type="password" placeholder="Your password" id="password" name="password" />
+            <button>Log In</button>
         </form>
     )
 }
